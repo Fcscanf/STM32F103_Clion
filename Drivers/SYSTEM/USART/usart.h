6b516204b7cbb187cc180e63c35f5cdd00faec71
Fcant <fcscanf@outlook.com>
2025-09-18 16:44:22 +0800

    :art: 移除未使用的导入

--- Drivers/SYSTEM/USART/usart.h
+++ Drivers/SYSTEM/USART/usart.h
@@ -5,7 +5,6 @@
 #ifndef STM32F103_CLION_UART_H
 #define STM32F103_CLION_UART_H
 #include "stm32f1xx_hal.h"
-#include "stm32f1xx_hal_uart.h"
 
 void USART1_UART_Init(uint32_t baudrate);
 void USART1_FeedBack(void);
