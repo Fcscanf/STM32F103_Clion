47b3a1f5aa1f5642a0ce7520de4fac773a940d4d
Fcant <fcscanf@outlook.com>
2025-11-11 22:59:23 +0800

    :sparkles: STM32F103 的内部温度传感器实验

--- Drivers/BSP/ADC/adc.c
+++ Drivers/BSP/ADC/adc.c
@@ -8,8 +8,8 @@
 
 ADC_HandleTypeDef g_adc_handle;
 
-/* ADC单通道 */
-void adc_init(void) {
+/* ADC 内部温度传感器 初始化函数 */
+void adc_temperature_init(void) {
     ADC_ChannelConfTypeDef adc_ch_conf;
 
     g_adc_handle.Instance = ADC1;
@@ -24,7 +24,7 @@ void adc_init(void) {
 
     HAL_ADCEx_Calibration_Start(&g_adc_handle);
 
-    adc_ch_conf.Channel = ADC_CHANNEL_1;
+    adc_ch_conf.Channel = ADC_CHANNEL_16;
     adc_ch_conf.Rank = ADC_REGULAR_RANK_1;
     adc_ch_conf.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
     HAL_ADC_ConfigChannel(&g_adc_handle, &adc_ch_conf);
@@ -33,15 +33,9 @@ void adc_init(void) {
 /* ADC MSP初始化函数 */
 void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc) {
     if (hadc->Instance == ADC1) {
-        GPIO_InitTypeDef gpio_init_struct;
         RCC_PeriphCLKInitTypeDef adc_clk_init = {0};
 
-        __HAL_RCC_GPIOA_CLK_ENABLE();
         __HAL_RCC_ADC1_CLK_ENABLE();
-
-        gpio_init_struct.Pin = GPIO_PIN_1;
-        gpio_init_struct.Mode = GPIO_MODE_ANALOG;
-        HAL_GPIO_Init(GPIOA, &gpio_init_struct);
 
         adc_clk_init.PeriphClockSelection = RCC_PERIPHCLK_ADC;
         adc_clk_init.AdcClockSelection = RCC_ADCPCLK2_DIV6;
@@ -56,22 +50,35 @@ uint32_t adc_get_result(void) {
     return (uint16_t) HAL_ADC_GetValue(&g_adc_handle);
 }
 
-uint16_t adcx;
-float temp;
+/* 获取内部温度传感器温度值 */
+short adc_get_temperature(void) {
+    uint32_t adcx;
+    short result;
+    double temperature;
 
-void show(void) {
     adcx = adc_get_result();
-    // lcd_show_xnum(134, 110, adcx, 5, 16, 0, BLUE);  /* 显示ADCC采样后的原始值 */
-    printf("S: %d\r\n", adcx);
+    temperature = adcx * (3.3 / 4096);
+    temperature = (1.43 - temperature) / 0.0043 + 25;
+    result = temperature *= 100;
+    return result;
+}
 
-    temp = (float) adcx * (3.3 / 4096); /* 获取计算后的带小数的实际电压值，比如3.1111 */
 
-    adcx = temp; /* 赋值整数部分给adcx变量，因为adcx为u16整形 */
-    // lcd_show_xnum(134, 130, adcx, 1, 16, 0, BLUE);  /* 显示电压值的整数部分，3.1111的话，这里就是显示3 */
-    printf("V: %d", adcx);
+short temp;
 
-    temp -= adcx; /* 把已经显示的整数部分去掉，留下小数部分，比如3.1111-3=0.1111 */
-    temp *= 1000; /* 小数部分乘以1000，例如：0.1111就转换为111.1，相当于保留三位小数。 */
-    // lcd_show_xnum(150, 130, temp, 3, 16, 0X80, BLUE);/* 显示小数部分（前面转换为了整形显示），这里显示的就是111. */
-    printf(".%d\r\n", (uint16_t) temp);
+void show_temperature(void) {
+    temp = adc_get_temperature(); /* 得到温度值 */
+    printf("Temperature: ");
+    if (temp < 0) {
+        temp = -temp;
+        // lcd_show_string(30 + 10 * 8, 120, 16, 16, 16, "-", BLUE);   /* 显示负号 */
+        printf("-");
+    } else {
+        // lcd_show_string(30 + 10 * 8, 120, 16, 16, 16, " ", BLUE);   /* 无符号 */
+        printf(" ");
+    }
+    // lcd_show_xnum(30 + 11 * 8, 120, temp / 100, 2, 16, 0, BLUE);    /* 显示整数部分 */
+    // lcd_show_xnum(30 + 14 * 8, 120, temp % 100, 2, 16, 0X80, BLUE); /* 显示小数部分 */
+    printf("%d.", temp / 100);
+    printf("%d\r\n", temp % 100);
 }
--- Drivers/BSP/ADC/adc.h
+++ Drivers/BSP/ADC/adc.h
@@ -7,8 +7,9 @@
 
 #include "stm32f1xx_hal.h"
 
-void adc_init(void);
-uint32_t adc_get_result(void);
-void show(void);
+void adc_temperature_init(void);
+// uint32_t adc_get_result(void);
+// short adc_get_temperature(void);
+void show_temperature(void);
 
 #endif //STM32F103_CLION_ADC_H
