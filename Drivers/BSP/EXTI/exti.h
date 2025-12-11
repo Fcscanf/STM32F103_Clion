b250721b552af870ab8cc0d11b7990a865538765
Fcant <fcscanf@outlook.com>
2025-12-11 22:31:00 +0800

    :construction: 摄像头OV7725实验(未下载验证)

---
+++ Drivers/BSP/EXTI/exti.h
@@ -0,0 +1,46 @@
+//
+// Created by fcant on 2025/12/11 星期四.
+//
+
+#ifndef STM32F103_CLION_EXTI_H
+#define STM32F103_CLION_EXTI_H
+
+#include "stm32f1xx_hal.h"
+
+/******************************************************************************************/
+/* 引脚 和 中断编号 & 中断服务函数 定义 */
+
+#define KEY0_INT_GPIO_PORT              GPIOE
+#define KEY0_INT_GPIO_PIN               GPIO_PIN_4
+#define KEY0_INT_GPIO_CLK_ENABLE()      do{ __HAL_RCC_GPIOE_CLK_ENABLE(); }while(0)   /* PE口时钟使能 */
+#define KEY0_INT_IRQn                   EXTI4_IRQn
+#define KEY0_INT_IRQHandler             EXTI4_IRQHandler
+
+#define KEY1_INT_GPIO_PORT              GPIOE
+#define KEY1_INT_GPIO_PIN               GPIO_PIN_3
+#define KEY1_INT_GPIO_CLK_ENABLE()      do{ __HAL_RCC_GPIOE_CLK_ENABLE(); }while(0)   /* PE口时钟使能 */
+#define KEY1_INT_IRQn                   EXTI3_IRQn
+#define KEY1_INT_IRQHandler             EXTI3_IRQHandler
+
+#define KEY2_INT_GPIO_PORT              GPIOE
+#define KEY2_INT_GPIO_PIN               GPIO_PIN_2
+#define KEY2_INT_GPIO_CLK_ENABLE()      do{ __HAL_RCC_GPIOE_CLK_ENABLE(); }while(0)   /* PE口时钟使能 */
+#define KEY2_INT_IRQn                   EXTI2_IRQn
+#define KEY2_INT_IRQHandler             EXTI2_IRQHandler
+
+#define WKUP_INT_GPIO_PORT              GPIOA
+#define WKUP_INT_GPIO_PIN               GPIO_PIN_0
+#define WKUP_INT_GPIO_CLK_ENABLE()      do{ __HAL_RCC_GPIOA_CLK_ENABLE(); }while(0)   /* PA口时钟使能 */
+#define WKUP_INT_IRQn                   EXTI0_IRQn
+#define WKUP_INT_IRQHandler             EXTI0_IRQHandler
+
+/* OV7725 VSYNC所在引脚 中断编号 & 中断服务函数 定义 */
+#define OV7725_VSYNC_INT_IRQn           EXTI9_5_IRQn
+#define OV7725_VSYNC_INT_IRQHandler     EXTI9_5_IRQHandler
+
+/******************************************************************************************/
+
+void extix_init(void);              /* 外部中断初始化 */
+void exti_ov7725_vsync_init(void);  /* OV7725 VSYNC所在引脚中断初始化 */
+
+#endif //STM32F103_CLION_EXTI_H
---
+++ Drivers/BSP/OV7725/sccb.h
@@ -0,0 +1,53 @@
+//
+// Created by fcant on 2025/12/11 星期四.
+//
+
+#ifndef STM32F103_CLION_SCCB_H
+#define STM32F103_CLION_SCCB_H
+
+#include "stm32f1xx_hal.h"
+
+/******************************************************************************************/
+/* 引脚 定义 */
+
+#define SCCB_SCL_GPIO_PORT               GPIOD
+#define SCCB_SCL_GPIO_PIN                GPIO_PIN_3
+#define SCCB_SCL_GPIO_CLK_ENABLE()       do{ __HAL_RCC_GPIOD_CLK_ENABLE(); }while(0)   /* PD口时钟使能 */
+
+#define SCCB_SDA_GPIO_PORT               GPIOG
+#define SCCB_SDA_GPIO_PIN                GPIO_PIN_13
+#define SCCB_SDA_GPIO_CLK_ENABLE()       do{ __HAL_RCC_GPIOG_CLK_ENABLE(); }while(0)   /* PG口时钟使能 */
+
+
+#define OV_SCCB_TYPE_NOD     0        /* 如果用不用开漏模式或SCCB上无上拉电阻，我们需要推挽和输入切换的方式 */
+#if OV_SCCB_TYPE_NOD
+    #define SCCB_SDA_IN()  {GPIOG->CRH&=0XFF0FFFFF;GPIOG->CRH|=0X00800000;}
+    #define SCCB_SDA_OUT() {GPIOG->CRH&=0XFF0FFFFF;GPIOG->CRH|=0X00300000;}
+#endif
+/******************************************************************************************/
+
+/* IO操作函数 */
+#define SCCB_SCL(x)   do{ x ? \
+HAL_GPIO_WritePin(SCCB_SCL_GPIO_PORT, SCCB_SCL_GPIO_PIN, GPIO_PIN_SET) : \
+HAL_GPIO_WritePin(SCCB_SCL_GPIO_PORT, SCCB_SCL_GPIO_PIN, GPIO_PIN_RESET); \
+}while(0)  /* SCL */
+
+#define SCCB_SDA(x)   do{ x ? \
+HAL_GPIO_WritePin(SCCB_SDA_GPIO_PORT, SCCB_SDA_GPIO_PIN, GPIO_PIN_SET) : \
+HAL_GPIO_WritePin(SCCB_SDA_GPIO_PORT, SCCB_SDA_GPIO_PIN, GPIO_PIN_RESET); \
+}while(0)  /* SDA */
+
+
+#define SCCB_READ_SDA       HAL_GPIO_ReadPin(SCCB_SDA_GPIO_PORT, SCCB_SDA_GPIO_PIN)     /* 读取SDA */
+
+
+/* 对外接口函数 */
+void sccb_init(void);
+void sccb_stop(void);
+void sccb_start(void);
+
+void sccb_nack(void);
+uint8_t sccb_read_byte(void);
+uint8_t sccb_send_byte(uint8_t data);
+
+#endif //STM32F103_CLION_SCCB_H
