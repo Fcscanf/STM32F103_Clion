8a4fff22b1e4a33c60f64b81525998ccc79b272c
Fcant <fcscanf@outlook.com>
2025-12-13 22:46:30 +0800

    :art: 代码格式化

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,10 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "../../BSP/LED/led.h"
+#include "../../BSP/KEY/key.h"
+#include "../../BSP/TIMER/atim.h"
+#include "../../Drivers/SYSTEM/USART/usart.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -163,10 +166,6 @@ static void GPIO_Init(void)
   /* USER CODE BEGIN MX_GPIO_Init_1 */
 
   /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOE_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
 
